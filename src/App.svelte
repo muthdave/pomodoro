aeba940c8684d3b1e77000d4775837591940082d
muthdave <david.muthentaler@gmail.com>
2025-09-08 15:26:10 +0200

    Fixed variable names, added functional EndTime-Calculation

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,65 @@
 <script>
-  console.log(Date.now());
-  var WorkMin = $state(25);
-  var PauseValue = $state(5);
+  let workMin = $state(25);
+  let pauseValue = $state(5);
+  let sessions = $state(3);
 
-  var PauseMin = $derived(Math.trunc(Math.ceil(PauseValue * 100) / 100));
-  var PauseSec = $derived(
+  let pauseMin = $derived(Math.trunc(Math.ceil(pauseValue * 100) / 100));
+  let pauseSec = $derived(
     Math.round(
       60 *
-        ((Math.ceil(PauseValue * 100) / 100) %
-          Math.trunc(Math.ceil(PauseValue * 100) / 100))
+        ((Math.ceil(pauseValue * 100) / 100) %
+          Math.trunc(Math.ceil(pauseValue * 100) / 100))
     )
   );
-  
+
+  let endTime = $derived.by(() => {
+    let time = Date.now();
+    const prepTime = 600;
+    let calcWorkSec = workMin * 60;
+    let calcPauseMin = pauseMin * 60 + pauseSec;
+    let longPauses = Math.trunc((sessions - 1) / 4);
+    let calcPauses = sessions - 1 - longPauses;
+
+    let calcEndTime = new Date(
+      time +
+        (prepTime +
+          calcWorkSec * sessions +
+          calcPauseMin * calcPauses +
+          longPauses * (calcPauseMin * 2.5)) *
+          1000
+    );
+
+    const hours = String(calcEndTime.getHours()).padStart(2, "0");
+    const minutes = String(calcEndTime.getMinutes()).padStart(2, "0");
+
+    return `${hours}:${minutes}`;
+  });
 </script>
 
 <main>
-  Work Duration: {WorkMin}<br />
+  Work Duration: {workMin}<br />
   <input
     type="range"
     defaultValue="25"
     step="5"
     min="15"
     max="55"
-    bind:value={WorkMin}
+    bind:value={workMin}
   /><br /><br />
-  Pause Duration {PauseMin}:{PauseSec}<br />
+  Pause Duration {pauseMin}:{pauseSec}<br />
   <input
     type="range"
     defaultValue="5"
     step="0.0833"
     min="3"
     max="8.008"
-    bind:value={PauseValue}
+    bind:value={pauseValue}
   /><br />
   Work Sessions<br />
-  <input type="number" defaultValue="3" min="1" /><br /><br />
+  <input type="number" bind:value={sessions} defaultValue="3" min="1" /><br
+  /><br />
+
+  <input type="time" bind:value={endTime} disabled/>
 </main>
 
 <style>
